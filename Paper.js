70305d12e37a025db66fb610789780513de9f4f7
shree42 <84387696+shree42@users.noreply.github.com>
2021-06-05 11:10:40 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,30 @@
+class Paper {
+  constructor(x, y) {
+    var options = {
+
+      isStatic:false,
+      
+        restitution:0.3,
+        friction:0,
+        density:1.2
+        
+        
+    }
+    this.body = Bodies.circle(x,y,20,options);
+   
+    
+
+    this.image=loadImage("paper.png")
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    
+    imageMode(CENTER);
+    image(this.image,0, 0, this.width, this.height);
+    pop();
+  }
+};
